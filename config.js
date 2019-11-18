f3de636fb4078c15c0d4fa47da4589dd3288275d
sunxh <396145270@qq.com>
2019-11-18 15:43:55 +0800

    1

---
+++ config.js
@@ -0,0 +1,4 @@
+
+var g_conf = {
+	baseUrl: "http://192.168.50.140:8080/wlzb"
+}
---
+++ precache-manifest.4df45e6fa847b141ce43438aa7df9d30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75417b597b395a597777e741895697f8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6522935eb4ae82252834",
+    "url": "./static/css/2.416b1b09.chunk.css"
+  },
+  {
+    "revision": "e8787b6fb2d07fd53d54",
+    "url": "./static/css/main.752f93b4.chunk.css"
+  },
+  {
+    "revision": "6522935eb4ae82252834",
+    "url": "./static/js/2.e510d327.chunk.js"
+  },
+  {
+    "revision": "e8787b6fb2d07fd53d54",
+    "url": "./static/js/main.794a96fc.chunk.js"
+  },
+  {
+    "revision": "b26aec1fbcd20535b8f8",
+    "url": "./static/js/runtime-main.02dadf93.js"
+  },
+  {
+    "revision": "56d603259d87785e61655a3e0804802a",
+    "url": "./static/media/zhuashou.56d60325.png"
+  }
+]);
